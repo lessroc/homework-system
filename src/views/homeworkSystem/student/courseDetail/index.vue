b7d0ff2f8dda0766c44cb7b4d06ec04af24ea7ef
lessroc <hpf98@qq.com>
2023-04-23 02:06:47 +0800

    feat: 增加学生课程详情页面

---
+++ src/views/homeworkSystem/student/courseDetail/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <PageWrapper>
+    <Card hoverable v-if="courseDetail.courseName" :style="{ cursor: 'default' }">
+      <template #cover>
+        <img alt="example" :src="courseDetail.coverUrl" class="coverUrl" />
+      </template>
+      <CardMeta :title="courseDetail.courseName">
+        <template #description>{{ courseDetail.courseDesc }}</template>
+      </CardMeta>
+      <HomeworkList :courseId="courseId" />
+    </Card>
+  </PageWrapper>
+</template>
+<script lang="ts" setup>
+  import { Card, CardMeta } from 'ant-design-vue';
+  import { onBeforeMount, ref } from 'vue';
+  import { PageWrapper } from '/@/components/Page';
+  import { getCourseDetailApi } from '/@/views/homeworkSystem/api/teacher';
+  import { useRoute } from 'vue-router';
+  import HomeworkList from '../homeworkList/index.vue';
+  const route = useRoute();
+  const courseId = route.params?.id ?? -1;
+  const getCourseDetail = async () => {
+    await getCourseDetailApi({ courseId: Number(courseId) }).then((res) => {
+      console.log('获取课程详情成功:', res);
+      courseDetail.value = res;
+    });
+  };
+
+  onBeforeMount(() => {
+    getCourseDetail();
+  });
+
+  const courseDetail = ref({});
+</script>
+
+<style lang="less" scoped>
+  .coverUrl {
+    width: 500px;
+    height: 400px;
+    margin: 0 auto;
+  }
+</style>

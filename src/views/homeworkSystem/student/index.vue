668f51d360b3b33972114945567243ddd9db98ba
lessroc <hpf98@qq.com>
2023-04-23 02:02:59 +0800

    perf: 优化学生课程列表页面

--- src/views/homeworkSystem/student/index.vue
+++ src/views/homeworkSystem/student/index.vue
@@ -2,19 +2,21 @@
   <PageWrapper>
     <Card
       class="courseContainer"
-      :tab-list="tabListNoTitle"
-      :active-tab-key="noTitleKey"
+      :tab-list="tabList"
+      :active-tab-key="tabKey"
       @tab-change="(key) => onTabChange(key)"
     >
-      <div v-if="noTitleKey === '我的课程'" class="courseListLayout"><MyCourses /></div>
-      <div v-else-if="noTitleKey === '全部课程'" class="courseListLayout">
-        <AllCourses :searchResult="searchResult" />
+      <div v-if="tabKey === '我的课程'" class="courseListLayout"><MyCourses /></div>
+      <div v-else-if="tabKey === '全部课程'" class="courseListLayout">
+        <AllCourses :searchKeyword="searchKeyword" />
       </div>
-      <template #tabBarExtraContent v-if="noTitleKey === '全部课程'">
+      <template #tabBarExtraContent>
         <a-input-search
+          v-if="tabKey === '全部课程'"
           v-model:value="value"
           placeholder="模糊搜索课程名称"
           enter-button
+          allowClear
           @search="onSearch"
         />
       </template>
@@ -23,12 +25,11 @@
 </template>
 <script lang="ts" setup>
   import { PageWrapper } from '/@/components/Page';
-  import { Card, message } from 'ant-design-vue';
+  import { Card } from 'ant-design-vue';
   import MyCourses from './courseList/MyCourses.vue';
   import AllCourses from './courseList/AllCourses.vue';
-  import { ref } from 'vue';
-  import { searchCourseApi } from '/@/views/homeworkSystem/api/student';
-  const tabListNoTitle = [
+  import { reactive, ref } from 'vue';
+  const tabList = [
     {
       key: '我的课程',
       tab: '我的课程',
@@ -38,67 +39,72 @@
       tab: '全部课程',
     },
   ];
-  const noTitleKey = ref('全部课程');
+  const tabKey = ref('我的课程');
 
   const onTabChange = (value: string) => {
-    noTitleKey.value = value;
+    tabKey.value = value;
+    console.log('切换标签:', value);
   };
   const value = ref('');
-  const searchResult = ref([]);
+  const searchKeyword = reactive({
+    keyword: '',
+    t: new Date().getTime(),
+  });
   const onSearch = (keyword: string) => {
     console.log('搜索:', keyword);
     keyword = keyword.trim();
-    if (keyword === '') {
-      message.error('搜索内容不能为空');
-      return;
-    }
-    // TODO: 搜索
-    searchCourseApi({ keyword })
-      .then((res) => {
-        console.log('搜索成功:', res);
-        searchResult.value = res;
-      })
-      .catch((err) => {
-        console.log('搜索失败:', err);
-      });
+    searchKeyword.keyword = keyword;
+    searchKeyword.t = new Date().getTime();
   };
 </script>
 <style lang="less" scoped>
   .courseContainer {
     height: calc(100vh - 80px - 32px);
-    overflow-y: scroll;
+    overflow: hidden;
+
+    & > :deep(.ant-card-head) {
+      height: 57px;
+    }
 
     & > :deep(.ant-card-body) {
-      height: calc(100% - 56.8px);
-      overflow-y: scroll;
+      height: calc(100% - 57px);
+      overflow: hidden;
       padding: 0;
 
       .courseListLayout {
-        // 网格布局
-        display: grid;
-        grid-template-columns: repeat(5, 1fr);
-        grid-gap: 16px;
-        padding: 16px;
+        height: 100%;
+        overflow: hidden;
 
-        .ant-card {
-          overflow: hidden;
+        .cardBox {
+          // 网格布局
+          display: grid;
+          grid-template-columns: repeat(5, 1fr);
+          grid-gap: 16px;
+          padding: 16px;
+          height: calc(100% - 64px);
+          overflow-y: auto;
 
-          .ant-card-body {
-            padding: 16px;
+          .ant-card {
+            height: fit-content;
 
-            .ant-card-meta-description {
-              overflow: hidden;
-              white-space: nowrap;
-              text-overflow: ellipsis;
+            .ant-card-body {
+              padding: 16px;
+
+              .ant-card-meta-description {
+                overflow: hidden;
+                white-space: nowrap;
+                text-overflow: ellipsis;
+              }
             }
-          }
 
-          .ant-card-actions {
-            & > li {
-              margin: 0;
+            .ant-card-actions {
+              & > li {
+                margin: 0;
 
-              .anticon-plus-circle {
-                padding: 12px 0;
+                .anticon-minus-circle,
+                .anticon-plus-circle {
+                  padding: 12px 0;
+                }
               }
             }
           }
@@ -108,6 +114,7 @@
           // 占满一行
           grid-column: 1 / -1;
           text-align: right;
+          padding: 16px;
         }
       }
     }

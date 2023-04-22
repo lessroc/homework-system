facd10d93ff915a98577556aaaa097dc13fec7d2
lessroc <hpf98@qq.com>
2023-04-23 02:05:42 +0800

    perf: 优化学生课程列表我的课程组件

--- src/views/homeworkSystem/student/courseList/MyCourses.vue
+++ src/views/homeworkSystem/student/courseList/MyCourses.vue
@@ -1,34 +1,45 @@
 <template>
-  <template v-for="item in 20" :key="item">
-    <CourseCard teacher-name="黄老师" :list="list" />
+  <template v-if="list.length > 0">
+    <div class="cardBox">
+      <template v-for="(item, i) in list" :key="i">
+        <CourseCard :cardInfo="item" :isAll="false" />
+      </template>
+    </div>
+    <Pagination class="paginationComponent" @set-paging="setPaging" :get-paging="paging" />
   </template>
-  <Pagination class="paginationComponent" @set-paging="setPaging" :get-paging="paging" />
+  <template v-else>
+    <div class="noData">暂无数据</div>
+  </template>
 </template>
 <script setup lang="ts">
   import CourseCard from '../components/CourseCard.vue';
   import Pagination from '../components/Pagination.vue';
   import { onBeforeMount, reactive } from 'vue';
-  import { getAllCourseListApi } from '/@/views/homeworkSystem/api/student';
+  import { getStuCourseListApi } from '/@/views/homeworkSystem/api/student';
+  import { useUserStore } from '/@/store/modules/user';
+  const userStore = useUserStore();
   let list = reactive([]);
   let paging = reactive({
     pageNum: 1,
-    pageSize: 10,
+    pageSize: 20,
     totalPage: 5,
-    total: 5,
+    total: 100,
   });
   const getList = async () => {
-    await getAllCourseListApi({
+    await getStuCourseListApi({
       pageNum: paging.pageNum,
       pageSize: paging.pageSize,
+      studentId: userStore.getUserInfo?.userId,
     })
       .then((res) => {
-        console.log('获取课程列表成功:', res);
+        console.log('获取学生课程列表成功:', res);
         list = Object.assign(list, res.list);
-        delete res.list;
-        paging = Object.assign(paging, res);
+        const { pageNum, pageSize, totalPage, total } = res;
+        const newPaging = { pageNum, pageSize, totalPage, total };
+        paging = Object.assign(paging, newPaging);
       })
       .catch((err) => {
-        console.log('获取课程列表失败:', err);
+        console.log('获取学生课程列表失败:', err);
       });
   };
   onBeforeMount(() => {

6714ca265797a5b274f57c7a54f6afb77ca20024
lessroc <hpf98@qq.com>
2023-04-23 02:02:13 +0800

    perf: 优化学生课程卡片

--- src/views/homeworkSystem/student/components/CourseCard.vue
+++ src/views/homeworkSystem/student/components/CourseCard.vue
@@ -1,23 +1,70 @@
 <template>
-  <Card hoverable style="cursor: default">
+  <Card
+    hoverable
+    :style="{ cursor: `${isAll ? 'default' : 'pointer'}` }"
+    @click="toCourseDetail(cardInfo.courseId)"
+  >
     <template #cover>
-      <!--<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />-->
-      <img
-        style="height: calc(100vh / 4)"
-        alt="example"
-        src="http://342j6q8933.wicp.vip/static/1681150460000最伟大的作品-周杰伦.jpg"
-      />
+      <img style="height: calc(100vh / 4)" alt="example" :src="cardInfo.coverUrl" />
     </template>
-    <CardMeta title="Europe Street beat" :description="`教师：${props.teacherName}`" />
+    <CardMeta :title="cardInfo.courseName" :description="`教师：${cardInfo.teacherName}`" />
     <template #actions>
-      <Tooltip color="blue" title="加入课程">
+      <Tooltip color="blue" title="加入课程" @click="joinCourse" v-if="isAll">
         <PlusCircleOutlined />
+      </Tooltip>
+      <Tooltip color="volcano" title="退出课程" @click="quitCourse" v-else>
+        <MinusCircleOutlined />
       </Tooltip>
     </template>
   </Card>
 </template>
 <script setup lang="ts">
-  import { Card, CardMeta, Tooltip } from 'ant-design-vue';
-  import { PlusCircleOutlined } from '@ant-design/icons-vue';
-  const props = defineProps<{ teacherName: string }>();
+  import { Card, CardMeta, message, Tooltip } from 'ant-design-vue';
+  import { PlusCircleOutlined, MinusCircleOutlined } from '@ant-design/icons-vue';
+  import { joinCourseApi, leaveCourseApi } from '/@/views/homeworkSystem/api/student';
+  import { useUserStore } from '/@/store/modules/user';
+  import { useGo } from '/@/hooks/web/usePage';
+  const go = useGo();
+
+  /**
+   * 打开课程作业列表页
+   */
+  const toCourseDetail = (courseId) => {
+    if (props.isAll) return;
+    go('/student/course-detail/' + courseId);
+  };
+  const userStore = useUserStore();
+  const props = defineProps<{ cardInfo: any; isAll: boolean }>();
+  const joinCourse = async () => {
+    console.log('加入课程');
+    await joinCourseApi({
+      courseId: props.cardInfo.courseId,
+      studentId: userStore.getUserInfo?.userId,
+    })
+      .then((res) => {
+        console.log('加入课程成功:', res);
+        message.success('加入课程成功');
+      })
+      .catch((err) => {
+        console.log('加入课程失败:', err);
+        message.error('加入课程失败');
+      });
+  };
+  const quitCourse = async () => {
+    console.log('退出课程');
+    await leaveCourseApi({
+      courseId: props.cardInfo.courseId,
+      studentId: userStore.getUserInfo?.userId,
+    })
+      .then((res) => {
+        console.log('退出课程成功:', res);
+        message.success('退出课程成功');
+        // 刷新页面
+        window.location.reload();
+      })
+      .catch((err) => {
+        console.log('退出课程失败:', err);
+        message.error('退出课程失败');
+      });
+  };
 </script>

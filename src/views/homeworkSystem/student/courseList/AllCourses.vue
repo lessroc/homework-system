4d3b529ff19b949165408a200ec8e23063cc5ab3
lessroc <hpf98@qq.com>
2023-04-23 02:04:09 +0800

    perf: 优化学生课程列表所有课程组件

--- src/views/homeworkSystem/student/courseList/AllCourses.vue
+++ src/views/homeworkSystem/student/courseList/AllCourses.vue
@@ -1,38 +1,79 @@
 <template>
-  <template v-for="item in 20" :key="item">
-    <CourseCard teacher-name="黄老师" :list="list" />
+  <template v-if="list.length > 0">
+    <div class="cardBox">
+      <template v-for="(item, i) in list" :key="i">
+        <CourseCard :cardInfo="item" :isAll="true" />
+      </template>
+    </div>
+    <Pagination class="paginationComponent" @set-paging="setPaging" :get-paging="paging" />
   </template>
-  <Pagination class="paginationComponent" @set-paging="setPaging" :get-paging="paging" />
+  <template v-else>
+    <div class="noData">暂无数据</div>
+  </template>
 </template>
 <script setup lang="ts">
   import CourseCard from '../components/CourseCard.vue';
   import Pagination from '../components/Pagination.vue';
-  import { onBeforeMount, reactive } from 'vue';
-  import { getAllCourseListApi } from '/@/views/homeworkSystem/api/student';
-  const props = defineProps<{
-    searchResult: any[];
-  }>();
-  console.log('子组件接收到的searchResult:', props.searchResult);
+  import { onBeforeMount, reactive, watch } from 'vue';
+  import { getAllCourseListApi, searchCourseApi } from '/@/views/homeworkSystem/api/student';
+  const props = defineProps<{ searchKeyword }>();
+  console.log('子组件接收到的keyword:', props.searchKeyword);
   let list = reactive([]);
   let paging = reactive({
     pageNum: 1,
-    pageSize: 10,
-    totalPage: 10,
+    pageSize: 20,
+    totalPage: 5,
     total: 100,
   });
+  // 监听父组件传来的搜索结果
+  watch(
+    () => props.searchKeyword,
+    (val) => {
+      console.log('子组件监听到父组件传来的搜索结果:', val);
+      paging.pageNum = 1;
+      paging.pageSize = 20;
+      if (val.keyword) {
+        getSearchResult();
+      } else {
+        getList();
+      }
+    },
+    {
+      deep: true,
+    },
+  );
+  const getSearchResult = async () => {
+    await searchCourseApi({
+      keyword: props.searchKeyword.keyword,
+      pageNum: paging.pageNum,
+      pageSize: paging.pageSize,
+    })
+      .then((res) => {
+        console.log('获取搜索结果成功:', res);
+        list.length = 0;
+        list = Object.assign(list, res.list);
+        const { pageNum, pageSize, totalPage, total } = res;
+        const newPaging = { pageNum, pageSize, totalPage, total };
+        paging = Object.assign(paging, newPaging);
+      })
+      .catch((err) => {
+        console.log('获取搜索结果失败:', err);
+      });
+  };
   const getList = async () => {
     await getAllCourseListApi({
       pageNum: paging.pageNum,
       pageSize: paging.pageSize,
     })
       .then((res) => {
-        console.log('获取课程列表成功:', res);
+        console.log('获取所有课程列表成功:', res);
         list = Object.assign(list, res.list);
-        delete res.list;
-        paging = Object.assign(paging, res);
+        const { pageNum, pageSize, totalPage, total } = res;
+        const newPaging = { pageNum, pageSize, totalPage, total };
+        paging = Object.assign(paging, newPaging);
       })
       .catch((err) => {
-        console.log('获取课程列表失败:', err);
+        console.log('获取所有课程列表失败:', err);
       });
   };
   onBeforeMount(() => {

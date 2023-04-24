c2df2e322cf10d9ba0860e6cd03d3d81dea0023c
lessroc <hpf98@qq.com>
2023-04-25 02:36:56 +0800

    feat: 增加管理员用户管理页面

---
+++ src/views/homeworkSystem/admin/UserManage.vue
@@ -0,0 +1,171 @@
+<template>
+  <Table
+    :columns="columns"
+    :data-source="userList.list"
+    :pagination="paginationProps"
+    :scroll="{ y: 'calc(100vh - 200px)' }"
+    :loading="loading"
+    bordered
+  >
+    <template #bodyCell="{ column, text, record }">
+      <template v-if="['userName', 'password'].includes(column.dataIndex)">
+        <div>
+          <Input
+            v-if="editableData[record.id]"
+            v-model:value="editableData[record.id][column.dataIndex]"
+            style="margin: -5px 0"
+          />
+          <template v-else>
+            {{ text }}
+          </template>
+        </div>
+      </template>
+      <template v-else-if="column.dataIndex === 'operation'">
+        <div class="editable-row-operations">
+          <span v-if="editableData[record.id]">
+            <TypographyLink @click="save(record.id)">保存</TypographyLink>
+            <Popconfirm title="确定取消？" @confirm="cancel(record.id)">
+              <a>取消</a>
+            </Popconfirm>
+          </span>
+          <span v-else>
+            <a @click="edit(record.id)">编辑</a>
+          </span>
+        </div>
+      </template>
+    </template>
+  </Table>
+</template>
+<script lang="ts" setup>
+  import { cloneDeep } from 'lodash-es';
+  import { onBeforeMount, reactive, ref } from 'vue';
+  import type { UnwrapRef } from 'vue';
+  import { Table, Input, TypographyLink, Popconfirm } from 'ant-design-vue';
+  import { getUserListApi } from '/@/views/homeworkSystem/api/admin';
+  import dayjs from 'dayjs';
+  const loading = ref<boolean>(false);
+  const props = defineProps({
+    userType: {
+      type: Number,
+      default: 1,
+    },
+  });
+
+  onBeforeMount(() => {
+    getUserList();
+  });
+
+  let userList = reactive({
+    pageNum: 1,
+    pageSize: 10,
+    totalPage: 0,
+    total: 0,
+    list: [] as any[],
+  });
+
+  const getUserList = async () => {
+    loading.value = true;
+    await getUserListApi({
+      pageNum: userList.pageNum,
+      pageSize: userList.pageSize,
+      userType: props.userType,
+    })
+      .then((res) => {
+        console.log('获取用户列表成功:', res);
+        // 时间格式化
+        res.list.forEach((item: any) => {
+          item.createTime = item.createTime && dayjs(item.createTime).format('YYYY-MM-DD HH:mm:ss');
+          item.updateTime = item.updateTime && dayjs(item.updateTime).format('YYYY-MM-DD HH:mm:ss');
+        });
+        userList = Object.assign(userList, res);
+      })
+      .finally(() => {
+        loading.value = false;
+      });
+  };
+
+  const paginationProps = reactive({
+    current: userList.pageNum,
+    pageSize: userList.pageSize,
+    total: userList.total,
+    showSizeChanger: true,
+    showQuickJumper: true,
+    showTotal: (total: number) => `共 ${total} 条`,
+    onChange: (page: number, pageSize: number) => {
+      console.log('当前页:', page, '每页条数:', pageSize);
+      userList.pageNum = page;
+      userList.pageSize = pageSize;
+      getUserList();
+    },
+    onShowSizeChange: (current: number, size: number) => {
+      console.log('当前页:', current, '每页条数:', size);
+      userList.pageNum = current;
+      userList.pageSize = size;
+      getUserList();
+    },
+  });
+
+  const columns = [
+    {
+      title: '用户ID',
+      dataIndex: 'id',
+      width: 100,
+      align: 'center',
+    },
+    {
+      title: '用户名',
+      dataIndex: 'userName',
+      width: 200,
+    },
+    {
+      title: '密码',
+      dataIndex: 'password',
+      width: 200,
+    },
+    {
+      title: '创建时间',
+      dataIndex: 'createTime',
+      width: 200,
+    },
+    {
+      title: '更新时间',
+      dataIndex: 'updateTime',
+      width: 200,
+    },
+    {
+      title: '操作',
+      dataIndex: 'operation',
+      width: 200,
+    },
+  ];
+  // 给columns元素添加居中
+  columns.forEach((item) => {
+    item.align = 'center';
+  });
+  interface DataItem {
+    createTime: string;
+    id: number;
+    updateTime: string;
+    password: string;
+    userName: string;
+  }
+
+  const editableData: UnwrapRef<Record<string, DataItem>> = reactive({});
+
+  const edit = (id: number) => {
+    editableData[id] = cloneDeep(userList.list.filter((item) => id === item.id)[0]);
+  };
+  const save = (id: number) => {
+    Object.assign(userList.list.filter((item) => id === item.id)[0], editableData[id]);
+    console.log('editableData[id]:save:', editableData[id]);
+    delete editableData[id];
+  };
+  const cancel = (id: number) => {
+    delete editableData[id];
+  };
+</script>
+<style scoped>
+  .editable-row-operations a {
+    margin-right: 8px;
+  }
+</style>

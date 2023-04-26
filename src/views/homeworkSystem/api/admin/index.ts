f35f20e581044d77c6af4271ad24aa580df9b953
lessroc <hpf98@qq.com>
2023-04-27 02:35:49 +0800

    feat: 增加管理员编辑课程接口

--- src/views/homeworkSystem/api/admin/index.ts
+++ src/views/homeworkSystem/api/admin/index.ts
@@ -3,6 +3,38 @@ import { homeworkHttp } from '/@/utils/http/axios';
 enum Api {
   getUserList = '/admin/userList',
   deleteUser = '/admin/user/delete',
+  getTeacherList = '/user/teacherList',
+  adminEditCourse = '/admin/edit',
+}
+
+/**
+ * 管理员编辑课程
+ */
+export function adminEditCourseApi(params: {
+  courseDesc: string;
+  courseId: number;
+  courseName: string;
+  coverUrl: string;
+  teacherId: number;
+}) {
+  return homeworkHttp.post(
+    {
+      url: Api.adminEditCourse,
+      params,
+    },
+    {
+      errorMessageMode: 'modal',
+    },
+  );
+}
+
+/**
+ * 获取教师列表
+ */
+export function getTeacherListApi() {
+  return homeworkHttp.get({
+    url: Api.getTeacherList,
+  });
 }
 
 /**

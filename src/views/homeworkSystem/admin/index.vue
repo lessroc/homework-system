a609eb875a1d13c069600e22b8e17101f0801db6
lessroc <hpf98@qq.com>
2023-04-27 02:39:12 +0800

    feat: 完善管理员课程管理所有功能

--- src/views/homeworkSystem/admin/index.vue
+++ src/views/homeworkSystem/admin/index.vue
@@ -1,73 +1,180 @@
 <template>
-  <Table
-    :columns="columns"
-    :data-source="allCoursesList.list"
-    :pagination="paginationProps"
-    :scroll="{ y: 'calc(100vh - 200px)' }"
-    :loading="loading"
-  >
-    <template #bodyCell="{ record, column }">
-      <template v-if="column.dataIndex === 'coverUrl'">
-        <img :src="record.coverUrl" class="coverImg" alt="" />
+  <div>
+    <Table
+      :columns="columns"
+      :data-source="allCoursesList.list"
+      :pagination="paginationProps"
+      :scroll="{ y: 'calc(100vh - 200px)' }"
+      :loading="loading"
+      bordered
+    >
+      <template #bodyCell="{ column, text, record }">
+        <template v-if="editField.includes(column.dataIndex)">
+          <template v-if="editableData[record.courseId]">
+            <div class="coverImgBox" v-if="column.dataIndex === 'teacherId'">
+              <Space>
+                {{ editableData[record.courseId][column.dataIndex] }}
+                <Button @click="visible = true">更换教师</Button>
+              </Space>
+            </div>
+            <div class="coverImgBox" v-else-if="column.dataIndex === 'teacherName'">
+              {{ editableData[record.courseId][column.dataIndex] }}
+            </div>
+            <!--上传图片-->
+            <div v-else-if="column.dataIndex === 'coverUrl'">
+              <Space>
+                <div class="coverImgBox">
+                  <img
+                    class="coverImg"
+                    :src="editableData[record.courseId][column.dataIndex]"
+                    :alt="editableData[record.courseId][column.dataIndex]"
+                  />
+                </div>
+                <Upload
+                  :show-upload-list="false"
+                  :before-upload="beforeUpload"
+                  :custom-request="customRequest"
+                >
+                  <Button>更换封面</Button>
+                </Upload>
+              </Space>
+            </div>
+            <Input
+              v-else
+              v-model:value="editableData[record.courseId][column.dataIndex]"
+              style="margin: -5px 0"
+            />
+          </template>
+          <div class="coverImgBox" v-else-if="column.dataIndex === 'coverUrl'">
+            <img class="coverImg" :src="record.coverUrl" :alt="record.coverUrl" />
+          </div>
+          <template v-else>
+            {{ text }}
+          </template>
+        </template>
+        <template v-else-if="column.dataIndex === 'operation'">
+          <div class="editable-row-operations">
+            <span v-if="editableData[record.courseId]">
+              <TypographyLink @click="save(record.courseId)">保存</TypographyLink>
+              <Popconfirm title="确定取消？" @confirm="cancel(record.courseId)">
+                <a>取消</a>
+              </Popconfirm>
+            </span>
+            <span v-else>
+              <a @click="edit(record.courseId)">编辑</a>
+              <Popconfirm title="确定删除该课程？" @confirm="deleteCourse(record.courseId)">
+                <a>删除</a>
+              </Popconfirm>
+            </span>
+          </div>
+        </template>
       </template>
-      <template v-if="column.dataIndex === 'operation'">
-        <Space>
-          <Button type="link" @click="editCourse(record)">编辑课程</Button>
-          <Popconfirm title="确定删除该课程？" @confirm="deleteCourse(record.courseId)">
-            <Button type="link">删除课程</Button>
-          </Popconfirm>
-        </Space>
-      </template>
-    </template>
-  </Table>
+    </Table>
+    <Modal
+      v-model:visible="visible"
+      title="选择任课教师"
+      centered
+      @ok="changeTeacher"
+      @cancel="cancelChangeTeacher"
+    >
+      <div class="teacherList">
+        <div
+          v-for="(item, i) in teacherList"
+          :key="item.userId"
+          @click="setTeacher(i, item)"
+          class="teacherItem"
+          :class="{ active: i === selectedTeachers.index }"
+        >
+          <div class="teacherInfo">
+            <div class="teacherId">{{ item.userId }}</div>
+            <div class="teacherName">{{ item.userName }}</div>
+          </div>
+        </div>
+      </div>
+    </Modal>
+  </div>
 </template>
 <script lang="ts" setup>
-  import { Table, Button, Popconfirm, Space } from 'ant-design-vue';
+  import { cloneDeep } from 'lodash-es';
   import { onBeforeMount, reactive, ref } from 'vue';
-  import { getAllCourseListApi } from '/@/views/homeworkSystem/api/student';
+  import type { UnwrapRef } from 'vue';
+  import {
+    Table,
+    Input,
+    TypographyLink,
+    Popconfirm,
+    Button,
+    Modal,
+    message,
+    Upload,
+    Space,
+  } from 'ant-design-vue';
   import { deleteCourseApi } from '/@/views/homeworkSystem/api/teacher';
+  import { getAllCourseListApi } from '/@/views/homeworkSystem/api/student';
+  import { adminEditCourseApi, getTeacherListApi } from '/@/views/homeworkSystem/api/admin';
+  import { uploadFileHomeworkApi } from '/@/views/homeworkSystem/api/sys/uploadFile';
   const loading = ref<boolean>(false);
-  const columns = [
-    {
-      title: '课程 ID',
-      dataIndex: 'courseId',
-      align: 'center',
-      width: 150,
-    },
-    {
-      title: '课程封面',
-      dataIndex: 'coverUrl',
-      align: 'center',
-      width: 232,
-    },
-    {
-      title: '课程名称',
-      dataIndex: 'courseName',
-      width: 150,
-    },
-    {
-      title: '课程描述',
-      dataIndex: 'courseDesc',
-    },
-    {
-      title: '教师 ID',
-      dataIndex: 'teacherId',
-      align: 'center',
-      width: 150,
-    },
-    {
-      title: '教师名称',
-      dataIndex: 'teacherName',
-      align: 'center',
-      width: 150,
-    },
-    {
-      title: '操作',
-      dataIndex: 'operation',
-      align: 'center',
-      width: 300,
-    },
-  ];
+  const visible = ref<boolean>(false);
+  const selectedTeachers = reactive<any>({
+    index: '',
+    userId: '',
+    userName: '',
+    currCourseId: '',
+  });
+  const editField = ['coverUrl', 'courseName', 'courseDesc', 'teacherId', 'teacherName'];
+
+  const beforeUpload = (file) => {
+    const isJPG = file.type === 'image/jpeg';
+    const isLt2M = file.size / 1024 / 1024 < 2;
+    if (!isJPG) {
+      message.error('上传封面只能是 JPG 格式!');
+    }
+    if (!isLt2M) {
+      message.error('上传封面大小不能超过 2MB!');
+    }
+    return isJPG && isLt2M;
+  };
+  const customRequest = ({ file, onSuccess, onError }) => {
+    const formData = new FormData();
+    formData.append('file', file);
+    uploadFileHomeworkApi(formData)
+      .then((res) => {
+        console.log('上传封面成功:', res);
+        editableData[selectedTeachers.currCourseId].coverUrl = res[0].fileUrl;
+        onSuccess(res);
+      })
+      .catch((err) => {
+        console.log('上传封面失败:', err);
+        onError(err);
+      });
+  };
+
+  const setTeacher = (i, item) => {
+    console.log('选择教师:', i, item);
+    selectedTeachers.index = i;
+    selectedTeachers.userId = item.userId;
+    selectedTeachers.userName = item.userName;
+  };
+
+  const cancelChangeTeacher = () => {
+    console.log('取消更换教师');
+    selectedTeachers.index = '';
+    selectedTeachers.userId = '';
+    selectedTeachers.userName = '';
+    visible.value = false;
+  };
+
+  // 更换教师
+  const changeTeacher = () => {
+    console.log('确定更换教师:', selectedTeachers);
+    if (selectedTeachers.userId && selectedTeachers.currCourseId) {
+      editableData[selectedTeachers.currCourseId].teacherId = selectedTeachers.userId;
+      editableData[selectedTeachers.currCourseId].teacherName = selectedTeachers.userName;
+      visible.value = false;
+    } else {
+      message.error('请选择任课教师');
+    }
+  };
 
   const deleteCourse = (courseId) => {
     console.log('删除课程Id:', courseId);
@@ -86,12 +193,27 @@
     pageSize: 10,
     totalPage: 0,
     total: 0,
-    list: [],
+    list: [] as any[],
   });
+
+  let teacherList = reactive({});
 
   onBeforeMount(() => {
     getAllCoursesList();
+    getTeacherList();
   });
+
+  const getTeacherList = () => {
+    console.log('获取教师列表');
+    getTeacherListApi()
+      .then((res) => {
+        console.log('获取教师列表成功:', res);
+        teacherList = Object.assign(teacherList, res);
+      })
+      .catch((err) => {
+        console.log('获取教师列表失败:', err);
+      });
+  };
 
   const getAllCoursesList = async () => {
     loading.value = true;
@@ -131,12 +253,188 @@
       getAllCoursesList();
     },
   });
+
+  const columns = [
+    {
+      title: '课程ID',
+      dataIndex: 'courseId',
+      align: 'center',
+      width: 80,
+    },
+    {
+      title: '课程封面',
+      dataIndex: 'coverUrl',
+      align: 'center',
+      width: 183,
+    },
+    {
+      title: '课程名称',
+      dataIndex: 'courseName',
+      width: 200,
+    },
+    {
+      title: '课程描述',
+      dataIndex: 'courseDesc',
+      width: 600,
+    },
+    {
+      title: '教师ID',
+      dataIndex: 'teacherId',
+      align: 'center',
+      width: 80,
+    },
+    {
+      title: '教师名称',
+      dataIndex: 'teacherName',
+      align: 'center',
+      width: 100,
+    },
+    {
+      title: '操作',
+      dataIndex: 'operation',
+      align: 'center',
+      width: 150,
+    },
+  ];
+
+  interface DataItem {
+    courseDesc: string;
+    courseId: number;
+    courseName: string;
+    coverUrl: string;
+    teacherId: number;
+    teacherName: string;
+  }
+
+  const editableData: UnwrapRef<Record<string, DataItem>> = reactive({});
+
+  const edit = (courseId: number) => {
+    selectedTeachers.currCourseId = courseId;
+    editableData[courseId] = cloneDeep(
+      allCoursesList.list.filter((item) => courseId === item.courseId)[0],
+    );
+  };
+  const save = (courseId: number) => {
+    Object.assign(
+      allCoursesList.list.filter((item) => courseId === item.courseId)[0],
+      editableData[courseId],
+    );
+    saveEditCourse(courseId);
+  };
+  const cancel = (courseId: number) => {
+    delete editableData[courseId];
+  };
+
+  // 保存编辑的课程
+  const saveEditCourse = (courseId: number) => {
+    console.log('保存编辑的课程:', editableData[courseId]);
+    const { courseName, courseDesc, coverUrl, teacherId } = editableData[courseId];
+    adminEditCourseApi({
+      courseId,
+      courseName,
+      courseDesc,
+      coverUrl,
+      teacherId,
+    })
+      .then((res) => {
+        console.log('保存编辑的课程成功:', res);
+        delete editableData[courseId];
+        // getAllCoursesList(); // 不用重新获取
+      })
+      .catch((err) => {
+        console.log('保存编辑的课程失败:', err);
+      });
+  };
 </script>
 <style lang="less" scoped>
-  .coverImg {
-    width: 200px;
-    height: 133px;
-    object-fit: contain;
-    border-radius: 4px;
+  .editable-row-operations a {
+    margin-right: 8px;
+  }
+
+  :deep(.ant-pagination) {
+    margin-right: 16px;
+  }
+
+  .coverImgBox {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    .coverImg {
+      height: 150px;
+      object-fit: cover;
+      border-radius: 4px;
+    }
+  }
+
+  .updateCoverImgBox {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .teacherList {
+    display: grid;
+    grid-template-columns: repeat(7, 1fr);
+    grid-gap: 8px;
+    padding: 16px;
+    max-height: 520px;
+    overflow-y: auto;
+
+    .teacherItem {
+      position: relative;
+      overflow: hidden;
+      padding-top: 100%;
+      border-radius: 4px;
+      cursor: pointer;
+      // 背景浅浅蓝色
+      background-color: #e6f7ff;
+      color: #1890ff;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: column;
+      transition: all 0.3s ease-in-out;
+
+      &:hover {
+        // 背景深蓝色
+        background-color: #bae7ff;
+        color: #096dd9;
+        box-shadow: 0 0 8px 0 rgba(0, 0, 0, 0.2);
+      }
+      // teacherItem 活跃
+      &.active {
+        // 背景深蓝色
+        background-color: #bae7ff;
+        color: #096dd9;
+        box-shadow: 0 0 8px 0 rgba(0, 0, 0, 0.2);
+      }
+
+      .teacherInfo {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+
+        .teacherId,
+        .teacherName {
+          width: 100%;
+          text-align: center;
+          // 超出显示省略号
+          overflow: hidden;
+          text-overflow: ellipsis;
+          white-space: nowrap;
+        }
+
+        .teacherId {
+          padding: 8px 8px 0;
+        }
+
+        .teacherName {
+          padding: 0 8px 8px;
+        }
+      }
+    }
   }
 </style>

cba099870c888897b16743718b41180296f8abe8
lessroc <hpf98@qq.com>
2023-04-29 14:51:26 +0800

    chore: 隐藏login页面logo

--- src/views/sys/login/Login.vue
+++ src/views/sys/login/Login.vue
@@ -16,7 +16,7 @@
     <div class="container relative h-full py-2 mx-auto sm:px-10">
       <div class="flex h-full">
         <div class="hidden min-h-full pl-4 mr-4 xl:flex xl:flex-col xl:w-6/12">
-          <AppLogo class="-enter-x" />
+          <!--<AppLogo class="-enter-x" />-->
           <div class="my-auto">
             <img
               :alt="title"

ece2532aeff27129a7bd980f39f780777b72daec
lessroc <hpf98@qq.com>
2023-05-01 03:00:55 +0800

    chore: 增加用户类型

--- src/views/homeworkSystem/userInfo/index.vue
+++ src/views/homeworkSystem/userInfo/index.vue
@@ -3,6 +3,9 @@
     <div class="pageContainer">
       <Space direction="vertical" :size="16" class="spaceStyle">
         <a-input-group compact class="flex">
+          <a-input :value="userTypeStr" disabled allowClear addonBefore="用户类型" />
+        </a-input-group>
+        <a-input-group compact class="flex">
           <a-input v-model:value="userName" allowClear addonBefore="修改昵称" />
           <a-button type="primary" @click="saveUserName">保存</a-button>
         </a-input-group>
@@ -17,7 +20,7 @@
 <script lang="ts" setup>
   import { PageWrapper } from '/@/components/Page';
   import { message, Space } from 'ant-design-vue';
-  import { ref } from 'vue';
+  import { computed, ref } from 'vue';
   import { useUserStore } from '/@/store/modules/user';
   import {
     editPasswordHomeworkApi,
@@ -28,6 +31,18 @@
   const password = ref<string>('');
   const userId = userStore.getUserInfo?.userId;
   const userType = userStore.getUserInfo?.userType;
+  const userTypeStr = computed(() => {
+    switch (userType) {
+      case 1:
+        return '学生';
+      case 2:
+        return '教师';
+      case 3:
+        return '管理员';
+      default:
+        return '未知';
+    }
+  });
   const saveUserName = () => {
     editUserNameHomeworkApi({
       userId,
